4081a2bba67c979bf7a4947fe33439e80e99b66f
Paweł <pawel@msd.info.pl>
2025-10-27 12:40:57 +0100

    DIP

---
+++ SOLID/DIP.cs
@@ -0,0 +1,87 @@
+﻿namespace SOLID
+{
+
+    interface IMessage
+    {
+        void Send();
+    }
+
+    class Sms : IMessage
+    {
+        public string PhoneNumber { get; set; }
+        public string Message { get; set; }
+
+        public void SendSms()
+        {
+            Console.WriteLine($"Sending SMS to {PhoneNumber}: {Message}");
+        }
+
+        public void Send()
+        {
+            SendSms();
+        }
+    }
+
+    class Mms : IMessage
+    {
+        public string PhoneNumber { get; set; }
+        public byte[] Message { get; set; }
+
+        public void SendMms()
+        {
+            Console.WriteLine($"Sending MMS to {PhoneNumber}");
+        }
+
+        public void Send()
+        {
+            SendMms();
+        }
+    }
+
+    class Email : IMessage
+    {
+        public string EmailAddress { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+
+        public void Send()
+        {
+            SendEmail();
+        }
+
+
+        public void SendEmail()
+        {
+            Console.WriteLine($"Sending Email to {EmailAddress}: {Subject}");
+        }
+    }
+
+
+
+    class Messanger
+    {
+        //wstrzykiwanie przez konstruktor
+        public Messanger(IEnumerable<IMessage?> messages)
+        {
+            Messages = messages;
+        }
+
+        //wstrzykiwanie przez zależność
+        public IEnumerable<IMessage?> Messages { get; set; }
+
+        //wstrzykiwanie przez metodę
+        public void Send(IEnumerable<IMessage> messages)
+        {
+            Messages = messages;
+            Send();
+        }
+
+        public void Send()
+        {
+            foreach (var message in Messages)
+            {
+                message?.Send();
+            }
+        }
+    }
+}
