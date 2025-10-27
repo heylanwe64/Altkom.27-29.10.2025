89be13422828d26c08656a9d525dabad08898504
Paweł <pawel@msd.info.pl>
2025-10-27 12:28:38 +0100

    ISP

---
+++ SOLID/ISP.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.I
+{
+    interface IExcelExporter
+    {
+        void ToExcel();
+    }
+    interface IPdfExporter
+    {
+        void ToPdf();
+    }
+
+    class Report : IExcelExporter, IPdfExporter
+    {
+        public void ToExcel()
+        {
+            // Implementacja eksportu do Excela
+        }
+        public void ToPdf()
+        {
+            // Implementacja eksportu do PDF
+        }
+    }
+
+    class Poem : IPdfExporter
+    {
+        public void ToPdf()
+        {
+            // Implementacja eksportu do PDF
+        }
+    }
+}
