277f96c4977c58e5cd5e284bf9eaab3f4829da2c
Paweł <pawel@msd.info.pl>
2025-10-28 11:11:12 +0100

    Bridge

---
+++ DesignPatterns/Structural/Bridge/Color.cs
@@ -0,0 +1,13 @@
+﻿namespace DesignPatterns.Structural.Bridge
+{
+    internal abstract class Color
+    {
+        public Brightness? Brightness { get; set; }
+
+
+        public override string ToString()
+        {
+            return Brightness is null ? GetType().Name : $"{Brightness} {GetType().Name}";
+        }
+    }
+}
