5b21f79b9dca9c68f235f76ff4964cfb33f28b58
Paweł <pawel@msd.info.pl>
2025-10-29 15:04:34 +0100

    Visitor

---
+++ DesignPatterns/Behavioral/Visitor/II/IElement.cs
@@ -0,0 +1,9 @@
+﻿using DesignPatterns.Behavioral.Visitor.I;
+
+namespace DesignPatterns.Behavioral.Visitor.II
+{
+    internal interface IElement
+    {
+        void Accept(IVisitor visitor);
+    }
+}
