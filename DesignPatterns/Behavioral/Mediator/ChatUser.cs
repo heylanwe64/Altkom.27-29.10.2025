63b3ffc71718d89159cbb6c08e7248a2332a909e
Paweł <pawel@msd.info.pl>
2025-10-29 10:54:06 +0100

    Mediator

---
+++ DesignPatterns/Behavioral/Mediator/ChatUser.cs
@@ -0,0 +1,13 @@
+﻿namespace DesignPatterns.Behavioral.Mediator
+{
+    internal class ChatUser : ChatMemeber
+    {
+        public override void Receive(string from, string message, bool isPrivate)
+        {
+            if (isPrivate)
+                Console.WriteLine($"{from} to {Nick}: {message}");
+            else
+                Console.WriteLine($"{from} to all: {message}");
+        }
+    }
+}
