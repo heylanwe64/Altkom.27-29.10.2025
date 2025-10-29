12a170bfcf2a9b52130335d01a54e7c32a9ec2ea
Paweł <pawel@msd.info.pl>
2025-10-29 13:09:49 +0100

    State

---
+++ DesignPatterns/Behavioral/State/CoffeeMachine.cs
@@ -0,0 +1,32 @@
+﻿namespace DesignPatterns.Behavioral.State
+{
+    internal class CoffeeMachine
+    {
+        private State _state;
+
+        public State State
+        {
+            get => _state;
+            set
+            {
+                _state = value;
+                System.Console.WriteLine($"State changed to: {_state.GetType().Name}");
+            }
+        }
+
+        public void PowerOn()
+        {
+            State = new HeatingUpState(this);
+        }
+
+        public void SmallCoffee()
+        {
+            _state.Small();
+        }
+
+        public void LargeCoffee()
+        {
+            _state.Large();
+        }
+    }
+}
---
+++ DesignPatterns/Behavioral/State/WorkingState.cs
@@ -0,0 +1,29 @@
+﻿namespace DesignPatterns.Behavioral.State
+{
+    internal class WorkingState : State
+    {
+        private readonly CancellationTokenSource _cancellationTokenSource;
+        public WorkingState(CoffeeMachine coffeeMachine, int time) : base(coffeeMachine)
+        {
+            _cancellationTokenSource = new CancellationTokenSource();
+            Task.Delay(TimeSpan.FromMilliseconds(time), _cancellationTokenSource.Token)
+                .ContinueWith(x =>
+                {
+                    CoffeeMachine.State = new IdleState(CoffeeMachine);
+                    _cancellationTokenSource.Dispose();
+                });
+        }
+
+        public override void Large()
+        {
+            Console.WriteLine("Cancelation...");
+            _cancellationTokenSource.Cancel();
+        }
+
+        public override void Small()
+        {
+            Console.WriteLine("Cancelation...");
+            _cancellationTokenSource.Cancel();
+        }
+    }
+}
